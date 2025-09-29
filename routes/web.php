a6a9b3e84a3c949e980bdd34ae748d1749baa6a0
LianPermadi <lianpermadi1@gmail.com>
2025-09-29 19:59:45 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\RoleController;
-use App\Http\Controllers\PengaturanWilayahController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\LogistikController;
 use App\Http\Controllers\BahanOlahanController;
